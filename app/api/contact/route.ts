35dccb82a54e5951a37e15279463618d4bcada21
sulaiman-khalil <sulaiman.kh@outlook.de>
2025-07-11 15:20:13 +0200

    change name kv

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,10 +4,10 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { name, email, phone, message } = body;
 
-  await fetch(process.env.UPSTASH_REST_URL!, {
+  await fetch(process.env.KV_REST_API_URL!, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
+      Authorization: `Bearer ${process.env.KV_REST_API_TOKEN}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
