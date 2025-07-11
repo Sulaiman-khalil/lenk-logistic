5cb9b35b3b2c55305ce2e1001c63c27a2f2f0b38
sulaiman-khalil <sulaiman.kh@outlook.de>
2025-07-11 15:27:43 +0200

    routes

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,24 +1,14 @@
+import { Redis } from "@upstash/redis";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+const redis = Redis.fromEnv();
+
+export const POST = async (request: Request) => {
   const { name, email, phone, message } = await request.json();
-
-  const url = process.env.KV_REST_API_URL!;
-  const token = process.env.KV_REST_API_TOKEN!;
   const key = `contact:${Date.now()}`;
-  const value = JSON.stringify({ name, email, phone, message });
+  const value = { name, email, phone, message };
 
-  const response = await fetch(
-    `${url}/set/${encodeURIComponent(key)}/${encodeURIComponent(value)}`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  await redis.set(key, value);
 
-  const result = await response.json();
-
-  return NextResponse.json({ status: "Kontakt gespeichert", result });
-}
+  return NextResponse.json({ status: "Kontakt gespeichert" });
+};
